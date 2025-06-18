a44037ca78425b080b22d88c16962346ce96720c
Mohammad Bahlaq <bahlaq57@gmail.com>
2025-06-18 11:44:18 +0300

    add some string practice

--- training/dealWithStrings.py
+++ training/dealWithStrings.py
@@ -1,23 +1,34 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 #calculate the length of string
-print ("length of banana: ")
+print (bcolors.HEADER + "length of banana: " + bcolors.ENDC)
 length = len ("banana")
 print (len)
 
 print ("\n")
 
 # check if subtext is in the text
-print ("Check if work [nana] is in [banana]")
+print (bcolors.HEADER + "Check if work [nana] is in [banana]" + bcolors.ENDC)
 print ("nana" in "banana")
 
 print ("\n")
 
 # check if subtext is in not the text
-print ("Check if work [nana] is not in [banana]")
+print (bcolors.HEADER + "Check if work [nana] is not in [banana]"  + bcolors.ENDC)
 print ("nana" in "banana")
 
 print ("\n")
 
-print ("Print all characters in banana")
+print (bcolors.HEADER + "Print all characters in banana"  + bcolors.ENDC)
 # The x will start from index 0
 num = 1
 for x in "banana":
@@ -32,29 +43,41 @@ b = "Hello, World!"
 print (b)
 print ("\n")
 
-print ("Slice from index 2 into 5")
+print (bcolors.HEADER + "Slice from index 2 into 5" + bcolors.ENDC)
 print (b[2:5]) # llo
 print ("\n")
 
-print ("Slice from index 0 into 5")
+print (bcolors.HEADER + "Slice from index 0 into 5" + bcolors.ENDC)
 print (b[:5]) # start from 0
 print ("\n")
 
-print ("Slice from index 2 until end")
+print (bcolors.HEADER + "Slice from index 2 until end" + bcolors.ENDC)
 print (b[2:]) # to the end of the string
 print ("\n")
 
-print ("Slice from index 2 until end by jump 2 steps each time")
+print (bcolors.HEADER + "Slice from index 2 until end by jump 2 steps each time" + bcolors.ENDC)
 print (b[2::2]) # two steps
 print ("\n")
 
-print ("Slice from index -3 to -5 [from the right to the left]")
+
+print (bcolors.HEADER + "Slice from index -3 to -5 [from the right to the left]" + bcolors.ENDC)
 print (b[-5:-2]) # from the right to the left
 print ("\n")
 
-print ("Make the text upper")
-print (b.upper)
+print (bcolors.HEADER + "Make the text upper" + bcolors.ENDC)
+print (b.upper())
 print ("\n")
 
-print ("Make the text lower")
-print (b.lower)
+print (bcolors.HEADER + "Make the text lower" + bcolors.ENDC)
+print (b.lower())
+print ("\n")
+
+print (bcolors.HEADER + "Delete Spaces at start and end" + bcolors.ENDC)
+b = "     Hello, World!     "
+print ("Delete: " + b.strip())
+print ("\n")
+
+print (bcolors.HEADER + "Replace Between two characters" + bcolors.ENDC)
+b = "Hello, World!"
+print ("Replace: " + b.replace('o', 'S'))
+print ("\n")
