65a4bbd94195b42ed575a18ce57dfa84ff45dfa5
Mohammad Bahlaq <bahlaq57@gmail.com>
2025-06-18 12:04:56 +0300

    Some of list functions

---
+++ training/list.py
@@ -0,0 +1,21 @@
+thislist = ["apple", "banana"]
+print (thislist)
+
+thislist[1] = "pineapple" # Change value
+print (thislist)
+
+thislist.append ("cherry") # Add item at the end of the list 
+print (thislist)
+
+thislist.insert (1, "orange") # Add item at specific index
+print (thislist)
+
+thislist.pop () # Remove the last item
+print (thislist)
+
+thislist.pop (1) # Remove specific index
+print (thislist)
+
+thislist.remove ("apple") # Remove item at specific value
+print (thislist)
+
