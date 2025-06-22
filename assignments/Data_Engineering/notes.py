d1409c41e6a1508c3981c1549aba12ff88ca608d
Mohammad Bahlaq <bahlaq57@gmail.com>
2025-06-22 09:51:39 +0300

    Add Data Engineering directory
    write some notes

---
+++ assignments/Data_Engineering/notes.py
@@ -0,0 +1,25 @@
+
+#?  Data Extraction:
+#*  retrieving raw data from various internal and external data sourc3es and moving it into a staging area or processing system
+
+#?  Doata Sources:
+#*  relational DB: MySQL, PostgreSQL\
+#*  flat files: CSV, Excel, JSON\
+#*  APIs: REST APIs\
+#*  IoT devices or sensors\
+#*  Cloud storage: AWS S3, Google Drive\
+#*  Logs and clickstreams: from websites or apps
+
+#! In Data Science: Garbage in Garbage out
+
+#? Popular websites for datasets
+#* Kaggle
+#   Huge collection of datasets for machine learning, competitions, and projects.
+#* UCI Machine Learning Repository
+#   Classic datasets for supervised and unsupervised learning.
+#*DAta.gov
+#   US goverment open data platform covering a wide range of topics, [accurate, actual data].
+#* Google Dataset Search
+#   A search enbgine to find datasets across the internet.
+#* WHO Global Health Observatory
+#   Health-related global datasets.
