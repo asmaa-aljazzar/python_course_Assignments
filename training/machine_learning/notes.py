95932d553b52b387d929075da2aa6ac0c37269e4
Mohammad Bahlaq <bahlaq57@gmail.com>
2025-06-29 11:19:07 +0300

    machine_learning folder created some notes on supervised and unsupervised learning

---
+++ training/machine_learning/notes.py
@@ -0,0 +1,60 @@
+
+# region
+#
+#* Artificial Intelligence [AI].
+#* Machine Learning [ML] -> is AI.
+#* Deep Learning [DL] -> is AI and ML.
+#* Generative AI [GAI] -> is AI and ML and DL.
+#
+# region
+#
+## AI at first work with posibilities [Rule base] - The Goal is #? to make the machine think and act like humans.
+#
+# region
+#
+#? Machine Learning: [Work with structure data]
+#*  is a discipline o fcomputer science theat uses computer algorithms and analytics to build predictive models that can solve proplems.
+#*  is  based on algorithms that can learn from data without relying on rule-based programming.
+#       Data => Learns from past data => output.
+#
+# region
+#
+#? Deep learning: [Work with structure and unstructure data]
+#*  is a subset of machine learning that deals with algorithms inspired by the structure and function of the human brain.
+#*  Can work with and enormous amount of both structured and unstructured data.
+#*  the major difference between DL and ML is the way data is presented to the machine.
+#       input . . . . => angorithm . . . . => output . . . .
+#       each dot in input is a data, each data will connect with each dot in algorithm, the same to output.
+#*  work with alot of dataset.    
+#
+# region
+#
+# Data Input => Data Cleaning => Pre-processing => Model Training => Deployment
+# Extarct    => Transform
+#
+# region
+#
+#? Machine Learning:
+#*       Supervised Learning: 
+#        I tell it the lable [survived or not], then it know the other and new data.
+#           - Classification. [Not ordered numbers]
+#               binary: tow things.
+#               multi.
+#               #! Lable: cat, dog.
+#               #! Classification: cat, cats, dog, dogs ==> 0, 1, 2, 3. || cant't change the number, can't do any operation on them.
+#           - Recognition. [Numbers]
+#               #! continuos data => data extract from data ==> the weight of a cat, the wight of a dog || If i do an poereation between them it will mean something
+#*       Unupervised Learning:
+#        I don't thell it the lables the AI will lable it.
+#*       Reinforcement Learning
+#
+# region
+#
+#? Supervised Learning:
+#   when do the algorithm and learn then have a data from a data set ==> modle.
+#? Unsupervised Learining
+# Finds patterns or groups in unlabeled data, like clustering or dimensionality reducion.
+# input raw data => Interpretation: unknown output => Algorithm => Processing => output
+#? reinforcement Learning
+# interacts with environment and learn from them based on #! rewards 
+# Action => env => Reward.
